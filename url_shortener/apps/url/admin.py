95e45dfb10d4a481fa51d59aae8b34d8094e34cf
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2019-06-14 06:25:33 +0700

    change last visit to date created last visit

--- url_shortener/apps/url/admin.py
+++ url_shortener/apps/url/admin.py
@@ -6,3 +6,6 @@ from url_shortener.apps.url.models import Url
 @admin.register(Url)
 class UrlAdmin(admin.ModelAdmin):
     list_display = ('slug', 'long_url', 'num_visit', 'last_visit')
+
+    def last_visit(self, obj: Url):
+        return obj.last_visit.date_created
