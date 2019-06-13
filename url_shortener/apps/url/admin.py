b5703d12754dd5db9ebda336ba30a58875c7eac0
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2019-06-14 06:29:51 +0700

    add check last_visit

--- url_shortener/apps/url/admin.py
+++ url_shortener/apps/url/admin.py
@@ -5,7 +5,10 @@ from url_shortener.apps.url.models import Url
 
 @admin.register(Url)
 class UrlAdmin(admin.ModelAdmin):
-    list_display = ('slug', 'long_url', 'num_visit', 'last_visit')
+    list_display = ('slug', 'long_url', 'num_visit', 'last_visit_date')
 
-    def last_visit(self, obj: Url):
+    def last_visit_date(self, obj: Url):
+        if obj.last_visit is None:
+            return 'No visit'
+
         return obj.last_visit.date_created
